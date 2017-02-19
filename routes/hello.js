8596be34959aaa272bd9daf3337f392ce843c08c
Robbie Pallas <robbie.pallas@piksel.com>
2017-02-19 12:36:40 +0000

    fix some comments

--- routes/hello.js
+++ routes/hello.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET hello listing. */
+/* GET hello. */
 router.get('/:name', function(req, res) {
   res.send(`hello ${req.params.name || ''}`);
 });

29eadfceb0589f9f24b9126d48824a0a2f9125e2
Robbie Pallas <robbie.pallas@piksel.com>
2017-02-19 12:36:40 +0000

    fix some comments

--- routes/goodbye.js
+++ routes/goodbye.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET goodbye listing. */
+/* GET goodbye. */
 router.get('/', function(req, res) {
   res.send(`goodbye`);
 });
 
-/* GET goodbye listing. */
+/* GET goodbye with name. */
 router.get('/:name', function(req, res) {
   res.send(`goodbye ${req.params.name}`);
 });
